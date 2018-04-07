514c9b76e4d11a06159e2e17055f1510bfbd7ff1
Bryan Reed <bryan87reed@gmail.com>
2018-04-07 14:09:15 -0700

    Finishes dice_roller function with string output return
    Need to adjust output for multiple kinds of dice at once, error catching for bad input, etc

--- dice.py
+++ dice.py
@@ -5,5 +5,5 @@ class Die:
     def __init__(self,sides):
         self.sides = sides
 
-    def roll(self,num_of_dice):
+    def roll(self):
         return random.randint(1,int(self.sides))
--- roll.py
+++ roll.py
@@ -2,24 +2,39 @@ import random
 from dice import Die
 
 def dice_roller(dice_input_string):
-    """ Creates dice objects from the Die class using user input
-        Input Example: '2d8 1d20 5d6'
+    """ 
+    Creates dice objects from the Die class using user input
+    Rolls the dice and outputs the results as a string
+
     """
     # List of all dice user wants to roll
     roll_list = roll_input.split(' ')
-
+    output = 'Diceroll result: '
     dice_dict = {}
     # Loop that creates dict of Die Obeject
     for dice in roll_list:
         #  {2:Die(20), 1:Die(6), 3:Die(8)}
         dice_list = dice.split('d')
         dice_dict.update({dice_list[0]:Die(dice_list[1])})
-    
+
+    dice_results = []
     for dice in dice_dict.items():
         # dice = (number of dice, Die object)
-        for dice_number in range(dice[0]):
-            dice[1].roll()
-    return
+        for dice_number in range(int(dice[0])):
+            dice_results.append(dice[1].roll())
+    i = 0
+    # Loop that creates results string
+    for result in dice_results:
+        if i == 0:
+            output += str(result)
+        elif i == len(dice_results):
+            output += "{}".format(result)
+        else:
+            output += " + {}".format(result)
+        i += 1
+    
+    output += " = {}".format(sum(dice_results))
+    return output
 
 
 print('Type "q" to quit')
@@ -31,4 +46,4 @@ while True:
     if roll_input == 'q':
         break
     else:
-        print('Diceroll result: ' + dice_roller(roll_input))
+        print(dice_roller(roll_input))
